957cf458c3bb8873907eee6ce2e69c5a16efcd1c
alwaysqi <zw_pku@163.com>
2018-02-12 21:53:53 +0800

    test2

--- test2.py
+++ test2.py
@@ -1,15 +1,3 @@
-list1 = [0,1,2,3]
-list2 = [0,11,2,3]
-
-def judge():
-    count = 0
-    for (m,n) in zip(pd_merge_p['should_date_x'],pd_merge_p['should_date_y']):
-        if m==n:
-            count+=0
-            continue
-        else:
-            print(m,n)
-    print (count)
-
-judge()
-
+L=['baby','ann','hanmei','lilei']
+L.sort(key=str.lower)
+print(L)
